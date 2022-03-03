6332511ed5a32f2ce034d8030e799a5b110ed94c
Ramsest01 <56605629+Ramsest01@users.noreply.github.com>
2022-03-03 15:12:12 -0400

    bd 030322

--- coneccion/coneccion.php
+++ coneccion/coneccion.php
@@ -1,9 +1,9 @@
 <?php  
-	$user = 'lgfzczwaihrgid';
-	$passwd = 'b0ef062096e9f2beb863f23e9c88ed2665c0c72249db3da2c5dee196a36ed253';
-	$db = 'dem44go1h3n0vm';
+	$user = 'imrvkduzdqqegk';
+	$passwd = '66495dac1f7055eff857a3bac9b16537aeb089a19d1bfde2d66f0d51c4d9e61f';
+	$db = 'd68h4ketfbl3vb';
 	$port = 5432;
-	$host = 'ec2-54-156-149-189.compute-1.amazonaws.com';
+	$host = 'ec2-34-206-245-175.compute-1.amazonaws.com';
 	$strCnx = "host=$host port=$port dbname=$db user=$user password=$passwd";
 	$cnx = pg_connect($strCnx) or die ("Error de conexion. ". pg_last_error());
 
